74bd456d954ed5763c11f5efe191000d7822b9a3
montumeena141 <montumeena141@gmail.com>
2025-02-27 12:32:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/** @format */
+
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    
+    arr.map((item) => {
+      if (item.profession === "developer") {
+        console.log(item);
+      }
+    });
+  }
+  
+  function PrintDeveloperbyForEach() {
+    
+  }
+  
+  function addData() {
+    
+  }
+  
+  function removeAdmin() {
+    
+  }
+  
+  function concatenateArray() {
+    
+  }
+  
+  
